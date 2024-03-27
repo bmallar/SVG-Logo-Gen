673e695309dd83e57009886cd7df34cb0af53f2d
bmallar <benmallar@gmail.com>
2024-03-27 18:34:36 -0500

    take out 4th test

--- tests/square.test.js
+++ tests/square.test.js
@@ -2,7 +2,7 @@ const Square = require('../lib/square.js')
 
 describe('Square',() => {
     test('If correct square tag is returned', () => {
-        const expectedSvg = `<square cx="150" cy="100" r="80" fill="yellow" />`
+        const expectedSvg = `<rect x="90" y="40" width="120" height="120" fill="yellow" />`
         const shape = new Square("yellow").render()
         expect(shape).toEqual(expectedSvg)
 }
--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -2,7 +2,7 @@ const Triangle = require('../lib/triangle.js')
 
 describe('Triangle',() => {
     test('If correct triangle tag is returned', () => {
-        const expectedSvg = `<triangle cx="150" cy="100" r="80" fill="red" />`
+        const expectedSvg = `<polygon points="150, 18 244, 182 56, 182" fill="red" />`
         const shape = new Triangle("red").render()
         expect(shape).toEqual(expectedSvg)
 }
